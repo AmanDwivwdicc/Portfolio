c36121084e1cee47d502164fb2040adf232a1106
AmanDwivwdicc <dwivediaman104@gmail.com>
2026-06-18 09:01:11 +0530

    changed some files

--- 01Portfolio/src/data/projects.js
+++ 01Portfolio/src/data/projects.js
@@ -2,7 +2,7 @@ const projects = [
 
   {
     title: "Turtle Animation Website",
-    image: "/public/turtle_animation.png",
+    image: "/turtle_animation.png",
     description:
       "A collection of beautiful Python Turtle animations integrated into a web experience.",
     link: "https://college-library-website-opal.vercel.app/"
@@ -10,7 +10,7 @@ const projects = [
 
   {
     title: "ShelfShare",
-    image: "/public/shelfshare.png",
+    image: "/shelfshare.png",
     description:
       "A platform for sharing books and study resources among students.",
     link: "https://github.com/AmanDwivwdicc/shelfshare"
@@ -18,7 +18,7 @@ const projects = [
 
   {
     title: "Love Proposal Website",
-    image: "/public/fun_love.png",
+    image: "/fun_love.png",
     description:
       "An interactive and playful proposal website with animations and user interaction.",
     link: "https://aman-loves-you.vercel.app/"
@@ -26,7 +26,7 @@ const projects = [
 
   {
     title: "Invisible Cloak",
-    image: "/public/invisible_cloak.png",
+    image: "/invisible_cloak.png",
     description:
       "A full-stack OpenCV project implementing a Harry Potter-style invisibility cloak.",
     link: "https://harry-potter-invisible-cloak.vercel.app/"
